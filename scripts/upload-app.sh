ec682818fa4ea05137e8dad96be12f3c4d2c42e4
mor.hazan <mor.hazan@experitest.com>
2018-07-23 18:55:56 +0300

    -v

--- scripts/upload-app.sh
+++ scripts/upload-app.sh
@@ -1,2 +1,2 @@
 pwd
-curl -k -X POST https://sales.experitest.com/api/v1/applications/new -H "Authorization: Bearer $accessKey" -H "Cache-Control: no-cache" -H "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F "file=/Users/distiller/project/app/build/Debug-iphoneos/ExperiBank.ipa"
+curl -k -v -X POST https://sales.experitest.com/api/v1/applications/new -H "Authorization: Bearer $accessKey" -H "Cache-Control: no-cache" -H "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F "file=/Users/distiller/project/app/build/Debug-iphoneos/ExperiBank.ipa"
