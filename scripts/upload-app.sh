24ad36215fe35b0259f4160670cfd1cf3e373705
mor.hazan <mor.hazan@experitest.com>
2018-07-23 15:54:26 +0300

    FilePath

--- scripts/upload-app.sh
+++ scripts/upload-app.sh
@@ -1,2 +1,2 @@
-APP_PATH=file=/Users/distiller/project/build/Debug-iphoneos/ExperiBank.ipa
+APP_PATH=file="/Users/distiller/project/build/Debug-iphoneos/ExperiBank.ipa"
 curl -k -X POST https://sales.experitest.com/api/v1/applications/new -H "Authorization: Bearer $accessKey" -H "Cache-Control: no-cache" -H "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F "$APP_PATH"
