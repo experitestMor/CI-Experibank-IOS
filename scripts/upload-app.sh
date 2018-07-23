d9a01b7b6c7d404b2d271987f4281bcdc09a49bb
mor.hazan <mor.hazan@experitest.com>
2018-07-23 19:28:35 +0300

    Admin

--- scripts/upload-app.sh
+++ scripts/upload-app.sh
@@ -1,2 +1,2 @@
 pwd
-curl -v https://sales.experitest.com/api/v1/applications/new -H "Authorization: Bearer $accessKey" -F "file=@/Users/distiller/project/build/Debug-iphoneos/ExperiBank.ipa"
+curl -v https://sales.experitest.com/api/v1/applications/new -H "Authorization: Bearer $accessKey" -H "project: Default" -F "file=@/Users/distiller/project/build/Debug-iphoneos/ExperiBank.ipa"
